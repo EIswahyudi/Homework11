9dfde87ecac89da608576945a8e97c41811a7a9c
Evan Iswahyudi <evan_iswahyu@yahoo.com>
2019-12-14 07:39:46 -0800

    edited2

--- employee-tracker-Schema.sql
+++ employee-tracker-Schema.sql
@@ -3,28 +3,29 @@ CREATE database employee_tracker_DB;
 USE employee_tracker_DB;
 
 CREATE TABLE department (
-
-    id INT NOT NULL,
-    name VARCHAR(30) NULL, 
-    PRIMARY KEY (id),
+	id int AUTO_INCREMENT NOT NULL, 
+    name varchar(40) NOT NULL, 
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-
-    id INT NOT NULL,
+    id int AUTO_INCREMENT NOT NULL, 
     title VARCHAR(30) NULL, 
     salary DECIMAL(10,4) NULL,
-    department_id INT NULL
-    PRIMARY KEY (id),
+    department_id INT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id),
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE employee(
-
+CREATE TABLE employee (
+	id int AUTO_INCREMENT NOT NULL, 
     first_name VARCHAR(30) NULL,
     last_name VARCHAR(30) NULL,
     role_id INT NULL,
-    manager_id INT NULL,
+    manager VARCHAR(30),
+	FOREIGN KEY (role_id) REFERENCES role(id),
     PRIMARY KEY (id)
+   
 );
 
 INSERT INTO department (name)
@@ -57,23 +58,39 @@ VALUES ("Legal Team Lead", 250000, 4);
 INSERT INTO role (title, salary, department_id)
 VALUES ("Lawyer", 190000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Doe", 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager)
+VALUES ("John", "Doe", 1, "Ashley Rodriguez");
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Nike", "Chan", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager)
+VALUES ("Mike", "Chan", 2, "John Doe");
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager)
 VALUES ("Ashley", "Rodriguez", 3, NULL);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Kevin", "Tupik", 4, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager)
+VALUES ("Kevin", "Tupik", 4, "Ashley Rodriguez");
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager)
 VALUES ("Malia", "Brown", 5, NULL);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager)
 VALUES ("Sarah", "Lourd", 6, NULL);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Tom", "Allen", 7, 4);
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
+
+SELECT e.id, e.first_name, e.last_name, r.title, d.name, r.salary, e.manager
+FROM employee e
+JOIN role r
+	ON e.role_id = r.id
+JOIN department d
+	ON d.id = r.department_id
+
+SELECT e.id, e.first_name, e.last_name, r.title, d.name
+FROM employee e
+JOIN role r
+    ON e.role_id = r.id
+JOIN department d
+    ON d.id = r.department_id
+
--- employee-tracker.js
+++ employee-tracker.js
@@ -2,44 +2,108 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "Bootcamps09!",
-  database: "employee_tracker_DB"
+    // Your password
+    password: "Bootcamps09!",
+    database: "employee_tracker_DB"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  promptUser();
+connection.connect(function (err) {
+    if (err) throw err;
+    promptUser();
 });
 
 function promptUser() {
-  inquirer
-    .prompt({
-      name: "action",
-      type: "list",
-      message: "What would you like to do?",
-      choices: [
-        "View All Employees",
-        "View All Employees By Department",
-        "View All Employees By Manager",
-        "Add Employee",
-        "Remove Employee",
-        "Update Employee Role",
-        "Update Employee Manager",
-      ]
-    })
-    .then(function (res) {
-        const user = res; 
-        answer.push(user);
-        
-        promptUser()
-    })
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View All Employees",
+                "View All Employees By Department",
+                "View All Employees By Manager",
+                "Add Employee",
+                "Remove Employee",
+                "Update Employee Role",
+                "Update Employee Manager",
+                "exit",
+            ]
+        })
+        .then(function (answer) {
+            switch (answer.action) {
+                case "View All Employees":
+                    viewemployees();
+                    break;
+
+                case "View All Employees By Department":
+                    viewemployeesdept();
+                    break;
+
+                case "View All Employees By Manager":
+                    viewemployeesmgr();
+                    break;
+
+                case "Add Employee":
+                    addemployee();
+                    break;
+
+                case "Remove Employee":
+                    removeemployee();
+                    break;
+
+                case "Update Employee Role":
+                    updateemployeerole();
+                    break;
+
+                case "Update Employee Manager":
+                    updateemployeemanager();
+                    break;
+
+                case "Exit":
+                    connection.end();
+                    break;
+            }
+        });
+}
+
+function viewemployees() {
+    var query = `SELECT e.id, e.first_name, e.last_name, r.title, d.name, r.salary, e.manager
+FROM employee e
+JOIN role r
+	ON e.role_id = r.id
+JOIN department d
+	ON d.id = r.department_id`
+
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+
+    console.table(res);
+
+    promptUser();
+  });
+}
+
+function viewemployeesdept() {
+    var query = `SELECT e.id, e.first_name, e.last_name, r.title, d.name
+FROM employee e
+JOIN role r
+    ON e.role_id = r.id
+JOIN department d
+    ON d.id = r.department_id`
+
+    connection.query(query, function (err, res) {
+    if (err) throw err;
+
+    console.table(res);
+
+    promptUser();
+  });
 }
